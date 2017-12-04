c55679223ed84cb32053533ad6e5351451be7d52
Aashish Radhakrishnan <Aashish_Radhakrishnan@student.uml.edu>
2017-12-04 13:05:27 -0500

    setting up for deployment

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides c
 
 // Export config object
 module.exports = {
-  uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
+  //uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
+  uri: 'mongodb://mrunmayee:Mrunmayee21@ds129796.mlab.com:29796/heroku_nqsq1nrd',
   secret: crypto, // Cryto-created secret
   db: 'mean-angular-2' // Database name
 }
