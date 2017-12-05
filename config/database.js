855a04b7c93a1de09d9d9cd69c1cb96e8c1eeb9c
Aashish Radhakrishnan <Aashish_Radhakrishnan@student.uml.edu>
2017-12-04 23:14:58 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,5 +5,5 @@ module.exports = {
   //uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
   uri: 'mongodb://mrunmayee:Mrunmayee21@ds129796.mlab.com:29796/heroku_nqsq1nrd',
   secret: crypto, // Cryto-created secret
-  db: 'mean-angular-2' // Database name
+  db: 'heroku_tsnbxsjr' // Database name
 }
